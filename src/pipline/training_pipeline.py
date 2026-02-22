5ae1ceebdb982858d9f7a129633b318152cba069
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-02-22 23:39:32 +0600

    feat: Add data validation component to the training pipeline, including schema-based structural checks for data integrity.

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -5,12 +5,16 @@ from src.logger import logging
 from src.entity.config_entity import DataIngestionConfig
 from src.entity.artifact_entity import DataIngestionArtifact
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
+from src.entity.config_entity import DataValidationConfig
+from src.entity.artifact_entity import DataValidationArtifact
 
 
 class TrainPipeline:
     
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -35,12 +39,34 @@ class TrainPipeline:
         except Exception as e:
             raise MyException(e, sys) from e    
 
+    def start_data_validation(self,data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+
+
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise MyException(e, sys) from e        
+
+
+
     def run_pipeline(self):
         try:
             logging.info("Entered the run_pipeline method of TrainPipeline class")
             logging.info("Starting the data ingestion component")
             data_ingestion_artifact = self.start_data_ingestion()
             logging.info("Data ingestion component completed")
+            logging.info("Starting the data validation component")
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            logging.info("Data validation component completed")
             logging.info("Exited the run_pipeline method of TrainPipeline class")
         except Exception as e:
             raise MyException(e, sys) from e        
